8d987e4faeb257f4907984682bae2ac6d9ae083c
Nada Amin <namin@alum.mit.edu>
2015-07-27 21:06:42 +0200

    check with -wp-rte

--- src/out/ex2.c
+++ src/out/ex2.c
@@ -1,9 +1,11 @@
 /*@ requires n > 0;
+    assigns \nothing;
     ensures 0 <= \result && \result < n;
 */
 int pick_index(int n) { return 0; }
 
 /*@ requires n > 0 && \valid(p+ (0..n-1));
+    assigns \nothing;
 */
 int pick_element(int* p, int n) {
   int i = pick_index(n);
@@ -13,6 +15,7 @@ int pick_element(int* p, int n) {
 
 /*@
   requires \valid(p);
+  assigns \nothing;
   ensures \result == p[0];
 */
 int pick_first(int* p) {
