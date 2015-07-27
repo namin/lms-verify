d17fda0c874a3bb0c77718471c2b03c1a88d17ad
Nada Amin <namin@alum.mit.edu>
2015-07-27 17:27:21 +0200

    the two files check identitically

--- src/out/ex2_bad.c
+++ src/out/ex2_bad.c
@@ -3,7 +3,7 @@
 */
 int pick_index(int n) { return 0; }
 
-/*@ requires n > 0 && \valid(p+ (0..n-1));
+/*@ requires n > 0;
 */
 int pick_element(int* p, int n) {
   int i = pick_index(n);
@@ -12,7 +12,6 @@ int pick_element(int* p, int n) {
 }
 
 /*@
-  requires \valid(p);
   ensures \result == p[0];
 */
 int pick_first(int* p) {
