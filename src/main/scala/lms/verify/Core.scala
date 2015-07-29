8e442104146d27385415a44d8f3283da6de18fec
Nada Amin <namin@alum.mit.edu>
2015-07-29 17:55:18 +0200

    work-around for bug in C code generation no longer needed after https://github.com/TiarkRompf/virtualization-lms-core/commit/5c30222601390483df4c77e59d6ecf6962f8e337

--- src/main/scala/lms/verify/Core.scala
+++ src/main/scala/lms/verify/Core.scala
@@ -92,11 +92,6 @@ trait Impl extends Dsl with VerifyOpsExp with ScalaOpsPkgExp with TupledFunction
         case _ => super.isPrimitiveType(tpe)
       }
     }
-    override def emitValDef(sym: Sym[Any], rhs: String): Unit = {
-      if (!isVoidType(sym.tp))
-        stream.println(remapWithRef(sym.tp) + quote(sym) + " = " + rhs + ";")
-      else stream.println(rhs + ";")
-    }
 
     def exprOfBlock(kw: String, e: Block[Boolean], m: Map[Sym[_], String] = Map()): String = {
       val r = exprOf(e.res, m)
