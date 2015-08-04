50d670c1e4e93b416eddb69756e92f4b8d1aa0c9
Nada Amin <namin@alum.mit.edu>
2015-08-04 17:13:27 +0200

    alternative formulation without return keyword inside loop

--- src/out/ex7.c
+++ src/out/ex7.c
@@ -15,3 +15,25 @@ int member(int* p, int n, int v) {
   }
   return -1;
 }
+
+/*@ requires n>0 && \valid(p+(0..n-1));
+    ensures \result==-1 ==> !(\exists int i; 0<=i<n && p[i]==v);
+    ensures \result!=-1 ==> 0 <= \result < n && p[\result]==v;
+    assigns \nothing;
+*/
+int member_noreturn(int* p, int n, int v) {
+  int r = -1;
+  /*@
+    loop invariant (0 <= i <= n);
+    loop invariant r==-1 ==> !(\exists int j; 0 <= j < i && p[j]==v);
+    loop invariant r!=-1 ==> 0 <= r < n && p[r]==v;
+    loop assigns i, r;
+    loop variant n-i;
+  */
+  for (int i=0; i<n; i++) {
+    if (r==-1 && p[i]==v) {
+      r = i;
+    }
+  }
+  return r;
+}
