a004ddf64f256f4d3ed6c23acd35179e8f0c35cc
Nada Amin <namin@alum.mit.edu>
2015-08-26 21:43:20 +0200

    low level matrix mult

--- src/out/lina1.check.c
+++ src/out/lina1.check.c
@@ -1,5 +1,5 @@
 /*@
-requires ((((((((x2>0) && (x3>0)) && (0<=x0)) && (x0<x2)) && (0<=x1)) && (x1<x1)) && (x2<100)) && (x3<100));
+requires ((((((((x2>0) && (x3>0)) && (0<=x0)) && (x0<x2)) && (0<=x1)) && (x1<x3)) && (x2<100)) && (x3<100));
 assigns \nothing;
 ensures ((0<=\result) && (\result<(x2*x3)));
 */
@@ -7,4 +7,40 @@ int index(int  x0, int  x1, int  x2, int  x3) {
   int x5 = x0 * x3;
   int x6 = x5 + x1;
   return x6;
+}
+/*@
+requires ((((((((((((((((x27>0) && (x28>0)) && (x30>0)) && (x34>0)) && (x28==x30)) && (x27==x33)) && (x34==x31)) && (x27<100)) && (x28<100)) && (x30<100)) && (x31<100)) && (x33<100)) && (x34<100)) && \valid(x26+(0..(x27*x28)-1))) && \valid(x29+(0..(x30*x31)-1))) && \valid(x32+(0..(x33*x34)-1)));
+*/
+void mm_mult(int  * x26, int  x27, int  x28, int  * x29, int  x30, int  x31, int  * x32, int  x33, int  x34) {
+  /*@
+  loop invariant (((x27==x33) && (0<=x37)) && (x37<=x27));
+  loop assigns x37, x32[(0..(x33*x34)-1)];
+  loop variant (x27-x37);
+  */
+  for(int x37=0; x37 < x27; x37++) {
+    /*@
+    loop invariant (((x34==x31) && (0<=x39)) && (x39<=x31));
+    loop assigns x39, x32[(0..(x33*x34)-1)];
+    loop variant (x31-x39);
+    */
+    for(int x39=0; x39 < x31; x39++) {
+      int x40 = index(x37,x39,x33,x34);
+      x32[x40] = 0;
+      /*@
+      loop invariant (((x30==x28) && (0<=x43)) && (x43<=x28));
+      loop assigns x43, x32[(0..(x33*x34)-1)];
+      loop variant (x28-x43);
+      */
+      for(int x43=0; x43 < x28; x43++) {
+        int x44 = x32[x40];
+        int x45 = index(x37,x43,x27,x28);
+        int x46 = x26[x45];
+        int x47 = index(x43,x39,x30,x31);
+        int x48 = x29[x47];
+        int x49 = x46 * x48;
+        int x50 = x44 + x49;
+        x32[x40] = x50;
+      }
+    }
+  }
 }
--- src/test/scala/lms/verify/LinearAlgebraTests.scala
+++ src/test/scala/lms/verify/LinearAlgebraTests.scala
@@ -9,10 +9,56 @@ class LinearAlgebraTests extends TestSuite {
       val index = toplevel("index",
         { (r: Rep[Int], c: Rep[Int], ra: Rep[Int], ca: Rep[Int]) => r*ca+c },
         { (r: Rep[Int], c: Rep[Int], ra: Rep[Int], ca: Rep[Int]) =>
-          ra > 0 && ca > 0 && 0 <= r && r < ra && 0 <= c && c < c &&
+          ra > 0 && ca > 0 && 0 <= r && r < ra && 0 <= c && c < ca &&
           ra < N && ca < N },
         { (r: Rep[Int], c: Rep[Int], ra: Rep[Int], ca: Rep[Int]) => result: Rep[Int] =>
           0 <= result && result < ra*ca })
+
+      val mm_mult = toplevel("mm_mult",
+        { (ma: Rep[Array[Int]], ra: Rep[Int], ca: Rep[Int],
+           mb: Rep[Array[Int]], rb: Rep[Int], cb: Rep[Int],
+           mc: Rep[Array[Int]], rc: Rep[Int], cc: Rep[Int]) =>
+          reflectMutableInput(mc)
+          loop(
+            {r: Rep[Int] => ra == rc && unit(0) <= r && r <= ra},
+            {r: Rep[Int] => List(r, mc within (0 until rc*cc))},
+            {r: Rep[Int] => ra-r}) {
+            for (r <- 0 until ra) {
+              loop(
+                {c: Rep[Int] => cc == cb && unit(0) <= c && c <= cb},
+                {c: Rep[Int] => List(c, mc within (0 until rc*cc))},
+                {c: Rep[Int] => cb-c}) {
+                for (c <- 0 until cb) {
+                  mc(index(r, c, rc, cc)) = 0
+                  loop(
+                    {i: Rep[Int] => rb==ca && unit(0) <= i && i <= ca},
+                    {i: Rep[Int] => List(i, mc within (0 until rc*cc))},
+                    {i: Rep[Int] => ca-i}) {
+                    for (i <- 0 until ca) {
+                      mc(index(r, c, rc, cc)) = mc(index(r, c, rc, cc)) + ma(index(r, i, ra, ca)) * mb(index(i, c, rb, cb))
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        { (ma: Rep[Array[Int]], ra: Rep[Int], ca: Rep[Int],
+           mb: Rep[Array[Int]], rb: Rep[Int], cb: Rep[Int],
+           mc: Rep[Array[Int]], rc: Rep[Int], cc: Rep[Int]) =>
+          ra > 0 && ca > 0 && rb > 0 && cc > 0 &&
+          ca == rb && ra == rc && cc == cb &&
+          ra < N && ca < N && rb < N && cb < N && rc < N && cc < N &&
+          valid(ma, 0 until ra*ca) &&
+          valid(mb, 0 until rb*cb) &&
+          valid(mc, 0 until rc*cc)
+        },
+        { (ma: Rep[Array[Int]], ra: Rep[Int], ca: Rep[Int],
+           mb: Rep[Array[Int]], rb: Rep[Int], cb: Rep[Int],
+           mc: Rep[Array[Int]], rc: Rep[Int], cc: Rep[Int]) =>
+          result: Rep[Unit] =>
+          unit(true)
+        })
     }
     check("1", (new Lina1 with Impl).code)
   }
