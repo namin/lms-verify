fb6cb910255094e359d205ce73ab9eb6a2cffdbc
Nada Amin <namin@alum.mit.edu>
2015-11-04 13:12:15 +0100

    hand-crafted atoi-like with overflow check

---
+++ src/out/parse1.c
@@ -0,0 +1,24 @@
+#include <string.h>
+
+/*@
+ requires strlen(a)>=0 && \valid(a+(0..strlen(a)));
+ assigns \nothing;
+ ensures 0<=\result || \result==-1;
+ */
+int my_atoi(char* a) {
+  int m = INT_MAX/10 - 10;
+  int r = 0;
+  char* s = a;
+  /*@
+    loop invariant strlen(s)>=0 && \valid(s+(0..strlen(s)));
+    loop invariant 0 <= r <= INT_MAX;
+    loop assigns r, s;
+    loop variant strlen(s);
+  */
+  while ('0' <= *s && *s <= '9') {
+    if (r >= m) return -1;
+    r = 10*r + (*s - '0');
+    s++;
+  }
+  return r;
+}
