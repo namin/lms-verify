e030abfdc665b7e5e4e18ab439f51f0d22d49da0
Nada Amin <nada.amin@epfl.ch>
2016-04-22 17:10:43 +0200

    whitespace cleanup

--- src/out/inv1.c
+++ src/out/inv1.c
@@ -1,17 +1,17 @@
 /*@
     // Invariant and equality spec for Vector[Char]
-    predicate spec_Inv_VC(char *a1, int n) = 
+    predicate spec_Inv_VC(char *a1, int n) =
       n==0 || (n>0 && \valid(a1+(0..n-1)));
     predicate spec_Eq_VC(char *a1, char *a2, int n1, int n2) =
       (n1==n2) &&
       (\forall int j; (0<=j<n1) ==> a1[j] == a2[j]);
     // Invariant and equality spec for Vector[Vector[Char]]
-    predicate spec_Inv_VVC(char **a1, int *an1, int n) = 
+    predicate spec_Inv_VVC(char **a1, int *an1, int n) =
       n==0 || (n>0 && \valid(a1+(0..n-1)) && \valid(an1+(0..n-1))) &&
       (\forall int j; (0<=j<n) ==> spec_Inv_VC(a1[j],an1[j]));
-    predicate spec_Eq_VVC(char **a1, char **a2, int *an1, int *an2, int n1, int n2) = 
+    predicate spec_Eq_VVC(char **a1, char **a2, int *an1, int *an2, int n1, int n2) =
       (n1==n2) &&
-      (\forall int j; (0<=j<n1) ==> spec_Eq_VC(a1[j],a2[j],an1[j],an2[j])); 
+      (\forall int j; (0<=j<n1) ==> spec_Eq_VC(a1[j],a2[j],an1[j],an2[j]));
 */
 // Contract and implementation for Vector[Char] equality
 /*@ requires spec_Inv_VC(a1,n1) && spec_Inv_VC(a2,n2);
@@ -39,7 +39,7 @@ int eq_VC(char *a1, char *a2, int n1, int n2) {
 int eq_VVC(char **a1, char **a2, int *an1, int *an2, int n1, int n2) {
   if (n1 != n2) return 0;
     /*@ loop invariant (0 <= i <= n1);
-        loop invariant \forall int j; (0 <= j < i) ==> spec_Eq_VC(a1[j],a2[j],an1[j],an2[j]); 
+        loop invariant \forall int j; (0 <= j < i) ==> spec_Eq_VC(a1[j],a2[j],an1[j],an2[j]);
         loop assigns i;
         loop variant (n1-i); */
     for (int i = 0; i < n1; i++) {
