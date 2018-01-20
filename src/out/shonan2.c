8ef9667c1cec2cf40a60646adb0c26ddcdbcf1d2
Nada Amin <namin@alum.mit.edu>
2018-01-20 05:30:13 +0000

    specialization checked

--- src/out/shonan2.c
+++ src/out/shonan2.c
@@ -39,3 +39,40 @@ void mv_mult_bool(int n, int *m, int *v, int *o) {
     }
   }
 }
+
+
+/*@
+requires n>0;
+requires n==2;
+
+requires \valid(m+(0..n*n-1));
+requires \valid(v+(0..n-1));
+requires \valid(o+(0..n-1));
+requires \separated(m+(0..n*n-1), v+(0..n-1), o+(0..n-1));
+
+requires m[0]==1;
+requires m[1]==1;
+requires m[2]==0;
+requires m[3]==0;
+
+assigns o[0..n-1];
+
+ensures \forall int i; 0 <= i < n ==>
+  o[i] == row_col_bool(n, i, n, m, v);
+*/
+void mv_mults_bool(int n, int *m, int *v, int *o) {
+  int r = 0;
+  o[r] = 0;
+  /*@
+    loop invariant 0 <= c <= n;
+    loop invariant 0 <= r < n;
+    loop invariant \forall int i; 0 <= i < r ==> o[i] == row_col_bool(n, i, n, m, v);
+    loop invariant o[r] == row_col_bool(c, r, n, m, v);
+    loop assigns c, o[r];
+    loop variant n-c;
+  */
+  for (int c = 0; c < n; c++) {
+    o[r] = o[r] || (m[n*r+c] && v[c]);
+  }
+  o[1] = 0;
+}
