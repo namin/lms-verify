7541a42513f0442fac7c35e577a809928fe05434
Nada Amin <namin@alum.mit.edu>
2018-01-20 17:24:51 +0000

    shonan version

--- src/out/shonan3.c
+++ src/out/shonan3.c
@@ -88,3 +88,110 @@ void mv_prod(int n, int *m, int *v, int *o) {
     put(o, r, t, n, m, v);
   }
 }
+
+/*@
+requires n>0;
+requires n==5;
+
+requires \valid(m+(0..n*n-1));
+requires \valid(v+(0..n-1));
+requires \valid(o+(0..n-1));
+requires \separated(m+(0..n*n-1), v+(0..n-1), o+(0..n-1));
+
+requires m[0]==1;
+requires m[1]==1;
+requires m[2]==1;
+requires m[3]==1;
+requires m[4]==1;
+
+requires m[5]==0;
+requires m[6]==0;
+requires m[7]==0;
+requires m[8]==0;
+requires m[9]==0;
+
+requires m[10]==0;
+requires m[11]==0;
+requires m[12]==1;
+requires m[13]==0;
+requires m[14]==0;
+
+requires m[15]==0;
+requires m[16]==0;
+requires m[17]==0;
+requires m[18]==0;
+requires m[19]==0;
+
+requires m[20]==0;
+requires m[21]==0;
+requires m[22]==1;
+requires m[23]==0;
+requires m[24]==1;
+
+requires \forall int i; 0 <= i < n ==> 0 <= v[i] <= 9;
+
+assigns o[0..n-1];
+
+ensures \forall int i; 0 <= i < n ==> o[i] == MV_Prod(n, i, n, m, v);
+*/
+void mv_prod_s(int n, int *m, int *v, int *o) {
+  int r = 0;
+  //@assert \forall int i; 0 <= i < r ==> o[i] == MV_Prod(n, i, n, m, v);
+  int t = 0;
+  /*@
+    loop invariant 0 <= c <= n;
+    loop invariant 0 <= r < n;
+    loop invariant \forall int i; 0 <= i < r ==> o[i] == MV_Prod(n, i, n, m, v);
+    loop invariant t == MV_Prod(c, r, n, m, v);
+    loop assigns c, t;
+    loop variant n-c;
+  */
+  for (int c = 0; c < n; c++) {
+    t = t + (m[c] * v[c]);
+  }
+  put(o, r, t, n, m, v);
+  r = 1;
+  //@assert \forall int i; 0 <= i < r ==> o[i] == MV_Prod(n, i, n, m, v);
+  t = 0;
+  //@assert t == MV_Prod(0, r, n, m, v);
+  //@assert t == MV_Prod(1, r, n, m, v);
+  //@assert t == MV_Prod(2, r, n, m, v);
+  //@assert t == MV_Prod(3, r, n, m, v);
+  //@assert t == MV_Prod(4, r, n, m, v);
+  //@assert t == MV_Prod(n, r, n, m, v);
+  put(o, r, t, n, m, v);
+  r = 2;
+  //@assert \forall int i; 0 <= i < r ==> o[i] == MV_Prod(n, i, n, m, v);
+  t = 0;
+  //@assert t == MV_Prod(0, r, n, m, v);
+  //@assert t == MV_Prod(1, r, n, m, v);
+  //@assert t == MV_Prod(2, r, n, m, v);
+  t += v[2];
+  //@assert t == MV_Prod(3, r, n, m, v);
+  //@assert t == MV_Prod(4, r, n, m, v);
+  //@assert t == MV_Prod(n, r, n, m, v);
+  put(o, r, t, n, m, v);
+  r = 3;
+  //@assert \forall int i; 0 <= i < r ==> o[i] == MV_Prod(n, i, n, m, v);
+  t = 0;
+  //@assert t == MV_Prod(0, r, n, m, v);
+  //@assert t == MV_Prod(1, r, n, m, v);
+  //@assert t == MV_Prod(2, r, n, m, v);
+  //@assert t == MV_Prod(3, r, n, m, v);
+  //@assert t == MV_Prod(4, r, n, m, v);
+  //@assert t == MV_Prod(n, r, n, m, v);
+  put(o, r, t, n, m, v);
+  r = 4;
+  //@assert \forall int i; 0 <= i < r ==> o[i] == MV_Prod(n, i, n, m, v);
+  t = 0;
+  //@assert t == MV_Prod(0, r, n, m, v);
+  //@assert t == MV_Prod(1, r, n, m, v);
+  //@assert t == MV_Prod(2, r, n, m, v);
+  t += v[2];
+  //@assert t == MV_Prod(3, r, n, m, v);
+  //@assert t == MV_Prod(4, r, n, m, v);
+  t += v[4];
+  //@assert t == MV_Prod(n, r, n, m, v);
+  put(o, r, t, n, m, v);
+  //@assert \forall int i; 0 <= i < n ==> o[i] == MV_Prod(n, i, n, m, v);
+}
