57ac342b15218d5e769a470c1f85ac8169e5f1b2
Nada Amin <na482@cl.cam.ac.uk>
2018-08-22 18:28:54 +0100

    refactor utils in prep of lib

---
+++ src/main/scala/lms/verify/utils.scala
@@ -0,0 +1,101 @@
+package lms.verify
+
+import java.io._
+
+object utils {
+  def readFile(name: String): String = {
+    try {
+      val buf = new Array[Byte](new File(name).length().toInt)
+      val fis = new FileInputStream(name)
+      fis.read(buf)
+      fis.close()
+      new String(buf)
+    } catch {
+      case e: IOException => ""
+    }
+  }
+  def writeFile(name: String, content: String) {
+    val out = new java.io.PrintWriter(new File(name))
+    out.write(content)
+    out.close()
+  }
+  def writeFileIndented(name: String, content: String) {
+    val out = new java.io.PrintWriter(new File(name))
+    printIndented(content)(out)
+    out.close()
+  }
+  def indent(str: String) = {
+    val s = new StringWriter
+    printIndented(str)(new PrintWriter(s))
+    s.toString
+  }
+  def printIndented(str: String)(out: PrintWriter): Unit = {
+    val lines = str.split("[\n\r]")
+    var indent = 0
+    for (l0 <- lines) {
+      val l = l0.trim
+      if (l.length > 0) {
+        var open = 0
+        var close = 0
+        var initClose = 0
+        var nonWsChar = false
+        l foreach {
+          case '{' => {
+            open += 1
+            if (!nonWsChar) {
+              nonWsChar = true
+              initClose = close
+            }
+          }
+          case '}' => close += 1
+          case x => if (!nonWsChar && !x.isWhitespace) {
+            nonWsChar = true
+            initClose = close
+          }
+        }
+        if (!nonWsChar) initClose = close
+        out.println("  " * (indent - initClose) + l)
+        indent += (open - close)
+      }
+    }
+    assert (indent==0, "indentation sanity check")
+  }
+
+  def time[A](a: => A) = {
+    val now = System.nanoTime
+    val result = a
+    val micros = (System.nanoTime - now) / 1000
+    println("%d microseconds".format(micros))
+    result
+  }
+  def captureOut(func: => Any): String = {
+    val source = new java.io.ByteArrayOutputStream()
+    withOutputFull(new java.io.PrintStream(source))(func)
+    source.toString
+  }
+  def withOutput[T](out: PrintStream)(f: => Unit): Unit = {
+    scala.Console.withOut(out)(scala.Console.withErr(out)(f))
+  }
+  def devnull(f: => Unit): Unit = {
+    withOutput(nullout)(f)
+  }
+  def nullout = new PrintStream(new OutputStream() {
+    override def write(b: Int) = {}
+    override def write(b: Array[Byte]) = {}
+    override def write(b: Array[Byte], off: Int, len: Int) = {}
+  })
+  def withOutputFull(out: PrintStream)(func: => Unit): Unit = {
+    val oldStdOut = System.out
+    val oldStdErr = System.err
+    try {
+      System.setOut(out)
+      System.setErr(out)
+      scala.Console.withOut(out)(scala.Console.withErr(out)(func))
+    } finally {
+      out.flush()
+      out.close()
+      System.setOut(oldStdOut)
+      System.setErr(oldStdErr)
+    }
+  }
+}
