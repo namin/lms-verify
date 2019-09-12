066be289b601d17ecf5902d4217e3d8a1461cd3f
Nada Amin <namin@alum.mit.edu>
2019-09-12 11:45:49 -0400

    proved

--- src/out/dfa_spec_aapb.c
+++ src/out/dfa_spec_aapb.c
@@ -43,7 +43,17 @@ int m_aapb(char* s) {
     //@assert s[1+n]!='A';
     //@assert star_A(s, 1, n);
     //@assert !star_A(s, 1, n+1);
-    //@assert !star_A(s, 1, strlen(s)-2); // need to show this
+    n++;
+    /*@
+      loop invariant 0 <= n <= strlen(s)-2;
+      loop invariant !star_A(s, 1, n);
+      loop assigns n;
+      loop variant strlen(s)-2-n;
+    */
+    while (n!=strlen(s)-2) {
+      n++;
+    }
+    //@assert !star_A(s, 1, strlen(s)-2);
     //@assert !match_aapb(s);
     return 0;
   }
