bf3c1b27f22e64234067a68cd6d703425d67ce83
Nada Amin <namin@alum.mit.edu>
2019-09-12 04:28:03 -0400

    comment out attempt to prove reverse direction

--- src/test/scala/lms/verify/NfaTests.scala
+++ src/test/scala/lms/verify/NfaTests.scala
@@ -287,11 +287,11 @@ trait DfaStagedLib extends DfaLib with StagedLib with Dfa2ReLib with Re2Pr {
         (bwd(r).map{x => matching(x, inp, 0, i)}.getOrElse(unit(false))) || b
       }
     def re_invariant(r: Int, inp: Rep[Input], i: Rep[Int]): Rep[Boolean] =
-      (bwd(r).map{x => matching(x, inp, 0, i)}.getOrElse(unit(false))) &&
+      (bwd(r).map{x => matching(x, inp, 0, i)}.getOrElse(unit(false))) /*&&
         r0n.foldLeft(unit(true)){(b,t) =>
           (if (t==r) unit(true)
           else (bwd(t).map{x => !matching(x, inp, 0, i)}.getOrElse(unit(true)))) && b
-        }
+        }*/
     def re_invariants(id: Rep[Int], inp: Rep[Input], i: Rep[Int]): Rep[Boolean] = r0n.foldLeft(unit(true)){(b,r) =>
       ((id == r) ==> (re_invariant(r, inp, i))) && b
     }
@@ -310,8 +310,8 @@ trait DfaStagedLib extends DfaLib with StagedLib with Dfa2ReLib with Re2Pr {
       requires(valid_input(inp))
       requires(inp.length<=Int.MaxValue) // to avoid overflow error in SPEC!
       ensures{(res: Rep[Boolean]) =>
-        (res ==> matching(re, inp, 0, inp.length)) &&
-        (matching(re, inp, 0, inp.length) ==> res)
+        (res ==> matching(re, inp, 0, inp.length)) /*&&
+        (matching(re, inp, 0, inp.length) ==> res)*/
       }
       var matched = true
       var id = 0
@@ -324,8 +324,8 @@ trait DfaStagedLib extends DfaLib with StagedLib with Dfa2ReLib with Re2Pr {
         valid_input(inp.to(i)) &&
         (matched ==> re_invariants(id, inp, i)) &&
         (matched ==> matching(re0, inp, 0, i)) &&
-        (matching(re0, inp, 0, i) ==> re_cover(inp, i)) &&
-        (!matched ==> !re_cover(inp, i)) &&
+        //(matching(re0, inp, 0, i) ==> re_cover(inp, i)) &&
+        //(!matched ==> !re_cover(inp, i)) &&
         finals_invariants(id, inp, i) &&
         id_invariant(id)),
         List[Any](cur, i, id, matched),
@@ -337,8 +337,8 @@ trait DfaStagedLib extends DfaLib with StagedLib with Dfa2ReLib with Re2Pr {
           matched = false
           r0n.foldLeft(unit(())){(b,r) =>
             if (!matched && id == r) {
-              _assert(re_invariants(id, inp, i))
-              _assert((id == r) ==> re_invariant(r, inp, i))
+              //_assert(re_invariants(id, inp, i))
+              //_assert((id == r) ==> re_invariant(r, inp, i))
               r0n.foldLeft(unit(())){(b,t) =>
                 val chars = dfa.transitions(r)(t)
                 if (chars.contains(c)) {
@@ -348,31 +348,18 @@ trait DfaStagedLib extends DfaLib with StagedLib with Dfa2ReLib with Re2Pr {
               }
             } else b
           }
-          if (!matched) {
-            r0n.foreach{t => r0n.foreach{r =>
-              if (id==r) {
-                _assert((id == r) ==> re_invariant(r, inp, i))
-                _assert(re_invariant(r, inp, i))
-                dfa.transitions(r)(t).foreach{a =>
-                  _assert(!(c == a))
-                }
-                _assert(!bwd(t).map{x => matching(x, inp, 0, i+1)}.getOrElse(unit(false)))
-              }
-            }}
-            _assert(!re_cover(inp, i+1))
-          }
-          _assert(!matched ==> !re_cover(inp, i+1))
-          _assert(!re_cover(inp, i+1) ==> !matching(re0, inp, 0, i+1))
-          _assert(matched ==> re_invariants(id, inp, i+1));
+          //_assert(!matched ==> !re_cover(inp, i+1))
+          //_assert(!re_cover(inp, i+1) ==> !matching(re0, inp, 0, i+1))
+          //_assert(matched ==> re_invariants(id, inp, i+1));
           i = ghost(ghost(i)+1) //TODO: could be inferred
           cur = cur.rest
-          _assert(matched ==> re_invariants(id, inp, i));
+          //_assert(matched ==> re_invariants(id, inp, i));
         }}
       val finalId = readVar(id)
       val res = cur.atEnd && matched && r0n.foldLeft(unit(false)){(b: Rep[Boolean],r: Int) => if (dfa.finals(r)) (r==finalId || b) else b}
-      _assert(matching(re0, inp, 0, i) ==> res)
-      _assert((cur.atEnd) ==> (i==n))
-      _assert(matching(re0, inp, 0, n) ==> res)
+      //_assert(matching(re0, inp, 0, i) ==> res)
+      //_assert((cur.atEnd) ==> (i==n))
+      //_assert((matching(re0, inp, 0, n) && r0n.foldLeft(unit(false)){(b: Rep[Boolean],r: Int) => if (dfa.finals(r)) (r==finalId || b) else b}) ==> res)
       res
     })
   }
